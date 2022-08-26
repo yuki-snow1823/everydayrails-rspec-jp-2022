36409f64853a8d1d5754fc4faa7851aecca5517c
horiyu <horikoshi@pepabo.com>
2022-08-26 09:44:35 +0900

    代用できるUserのvalidテストの削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with a first name, last name and email, and password" do
-    user = User.new(
-      first_name: "Aaron",
-      last_name:  "Sumner",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    expect(user).to be_valid
-  end
-
   it "has a valid factory" do
     expect(FactoryBot.build(:user)).to be_valid
   end
