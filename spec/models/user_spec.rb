5e18eb5ab652dedff915d0aaee4b51df5706d40c
horiyu <horikoshi@pepabo.com>
2022-08-26 09:02:09 +0900

    ファクトリへの書き換え（片方のユーザーは保存の必要なし

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,12 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
@@ -30,18 +34,8 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid with a duplicate email address" do
-    User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    user = User.new(
-      first_name:  "Jane",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
