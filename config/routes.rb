0bc2681ca4b99fa46b7fb416f467b106510cd699
horiyu <horikoshi@pepabo.com>
2022-08-31 11:57:01 +0900

    newとeditのテスト追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    resources :projects#, only: [:index, :show, :create]
+    resources :projects #, only: [:index, :show, :create]
   end
 
   root "home#index"
